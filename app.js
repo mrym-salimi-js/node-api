01ad98853333391a2a9f4cb2602db3d21bbf5966
Maryam Salimi <salimi.5134@gmail.com>
2024-08-30 09:41:20 +0330

    some changes

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const adsRiutes = require('./routes/adsRoutes');
+
 const app = express();
 
+// MIDDLEWARE:
+
+// --JASONPARSER
 app.use(express.json());
-const adsRiutes = require('./routes/adsRoutes');
+
+// --Routes
 app.use('/api/ads/', adsRiutes);
 
-console.log('hi from app');
+// ---404 ERROR (NOT FOUND) => EXIST IN REACT APP
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: 'آدرس معتبر نمی باشد',
+  });
+});
 
 module.exports = app;
