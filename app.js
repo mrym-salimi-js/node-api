c7f5224276df8a6f3cf94a5e0bc50e1517ed5e5a
Maryam Salimi <salimi.5134@gmail.com>
2024-10-13 17:32:44 +0330

    CORS control

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ const app = express();
 // --JASONPARSER
 app.use(express.json());
 
+// --CORS policy
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 // --Routes
 app.use('/api/ads/', adsRiutes);
 
