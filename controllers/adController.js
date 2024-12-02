79d2079a270d3fc8b18b3281fee0fb8a961582fb
Maryam Salimi <salimi.5134@gmail.com>
2024-12-02 23:55:51 +0330

    completing get ad by category function

--- controllers/adController.js
+++ controllers/adController.js
@@ -1,7 +1,7 @@
 const Ad = require('../models/adModel');
 const path = require('path');
 const fs = require('fs-extra');
-const { error } = require('console');
+const { error, time } = require('console');
 const User = require('../models/userModel');
 
 exports.getAllAd = async (req, res, next) => {
@@ -139,14 +139,6 @@ exports.updateAd = async (req, res, next) => {
 
 exports.getAdsByCategory = async (req, res, next) => {
   try {
-    let sortOptions;
-    // { name: 'جدیدترین', slug: 'n' },
-    // { name: 'ارزان‌ترین', slug: 'ch' }, ---> price
-    // { name: 'گران‌ترین', slug: 'ex' },
-    // { name: 'کم کارکردترین', slug: 'lp' }, ----> km
-    // { name: 'پرکارکردترین', slug: 'hp' },
-    // { name: 'قدیمی‌ترین سال', slug: 'oy' }, ----> year
-    // { name: 'جدیدترین سال', slug: 'ny' },
     if (req.query.o !== undefined) {
       req.query.o === 'n' && (sortOptions = { createAd: -1 });
       req.query.o === 'ch' && (sortOptions = { lable: 1 });
@@ -242,82 +234,79 @@ exports.getAdsByCategory = async (req, res, next) => {
         });
         ads = mnQuery;
       }
+
+      if (queryKey.startsWith('o')) {
+        //{ name: 'جدیدترین', slug: 'n' }
+        //{ name: 'ارزان ترین', slug: 'ch' }
+        //{ name: 'گران ترین', slug: 'ex' }
+        // { name: 'کم کارکردترین', slug: 'lp' }, ----> km
+        // { name: 'پرکارکردترین', slug: 'hp' },
+        // { name: 'قدیمی‌ترین سال', slug: 'oy' }, ----> year
+        // { name: 'جدیدترین سال', slug: 'ny' },
+        queries[queryKey] === 'n' &&
+          ads.sort((timeA, timeB) => {
+            return timeA - timeB;
+          });
+        (queries[queryKey] === 'ch' || queries[queryKey] === 'ex') &&
+          ads.sort((a, b) => {
+            const priceA = parseInt(
+              a.attribute.find(
+                (attr) =>
+                  attr.id === 1 || attr.id === 68090 || attr.id === 68093,
+              ).lable,
+              10,
+            );
+            const priceB = parseInt(
+              b.attribute.find(
+                (attr) =>
+                  attr.id === 1 || attr.id === 68090 || attr.id === 68093,
+              ).lable,
+              10,
+            );
+            return queries[queryKey].startsWith('ch')
+              ? priceA - priceB
+              : priceB - priceA;
+          });
+        (queries[queryKey] === 'lp' || queries[queryKey] === 'hp') &&
+          ads.sort((a, b) => {
+            const priceA = parseInt(
+              a.attribute.find((attr) => attr.name === 'کیلومتر').lable,
+              10,
+            );
+            const priceB = parseInt(
+              b.attribute.find((attr) => attr.name === 'کیلومتر').lable,
+              10,
+            );
+            return queries[queryKey].startsWith('lp')
+              ? priceA - priceB
+              : priceB - priceA;
+          });
+        (queries[queryKey] === 'oy' || queries[queryKey] === 'ny') &&
+          ads.sort((a, b) => {
+            const priceA = parseInt(
+              a.attribute.find((attr) => attr.name === 'سن بنا').lableId,
+              10,
+            );
+            const priceB = parseInt(
+              b.attribute.find((attr) => attr.name === 'سن بنا').lableId,
+              10,
+            );
+            return queries[queryKey].startsWith('ny')
+              ? priceA - priceB
+              : priceB - priceA;
+          });
+      }
+
+      if (queryKey.startsWith('wp')) {
+        queries['wp'] === 'true'
+          ? (ads = ads.filter((ad) => {
+              return ad.photo[0] && ad;
+            }))
+          : (ads = ads.filter((ad) => {
+              return !ad.photo[0] && ad;
+            }));
+      }
     }
-
-    // mx68101 => حداکثر سال تولید
-    // mx97023 => --
-    // mx68140 => --
-    // mx68102 => حداکثر کیلومتر
-    // mx97024 => --
-    // mx68141 => --
-    // mx68085 => حداکثر متراژ
-    // mx68090 => حداکثر رهن
-    // mx68091 => --
-    // mx68092 => حداکثر اجاره
-    // mx68093 => --
-    // mx70020 => حداکثر اجاره روزانه
-
-    // let mxMnElems;
-    // Object.keys(req.query).map((key) => {
-    //   if (key.startsWith('mx')) {
-    //     key === 'mx45213' &&
-    //       (mxMnElems = {
-    //         $elemMatch: {
-    //           name: 'متراژ',
-    //           lable: {
-    //             $lt: parseInt(queries[key]),
-    //           },
-    //         },
-    //       });
-    //   }
-    // });
-
-    // ads = await Ad.find({
-
-    //   // $and: Object.keys(queries).map((key) => {
-    //   //   if (key.startsWith('mx')) {
-    //   //     key === 'mx45213' && {
-    //   //       attribute: {
-    //   //         $elemMatch: {
-    //   //           name: 'متراژ',
-    //   //           lable: {
-    //   //             $lt: parseInt(queries[key]),
-    //   //           },
-    //   //         },
-    //   //       },
-    //   //     };
-    //   //   }
-    //   // }),
-
-    //   // attribute: {
-    //   //   $elemMatch: { lableId: { $in: aQueries } },
-    //   //   $elemMatch: {
-    //   //     name: 'قیمت (تومان)',
-    //   //     lable: {
-    //   //       $lt: req.query.mx45213 ? parseInt(req.query.mx45213) : 0,
-    //   //     },
-    //   //   },
-    //   //   $elemMatch: {
-    //   //     name: 'متراژ',
-    //   //     lable: {
-    //   //       $lt: req.query.mx8888 ? parseInt(req.query.mx8888) : 0,
-    //   //     },
-    //   //   },
-    //   // },
-    // });
-
-    // const ads = await Ad.aggregate([
-    //   {
-    //     $match: {
-    //       category: { $elemMatch: { slug: req.params.category } },
-    //       location: {
-    //         $elemMatch: {
-    //           id: { $in: JSON.parse(decodeURIComponent(req.query.cities)) },
-    //         },
-    //       },
-    //     },
-    //   },
-    // ]);
 
     res.status(200).json({
       status: 'success',
@@ -331,25 +320,3 @@ exports.getAdsByCategory = async (req, res, next) => {
     });
   }
 };
-
-// exports.getAdsByFilter = async (req, res, next) => {
-//   try {
-//     console.log(req.query);
-//     const filters = req.query;
-
-//     // const ads = await Ad.find({
-//     //   category: { $elemMatch: { slug: req.params.category } },
-//     // });
-
-//     // res.status(200).json({
-//     //   status: 'success',
-//     //   result: ads.length,
-//     //   data: ads,
-//     // });
-//   } catch (error) {
-//     res.status(404).json({
-//       status: 'fail',
-//       message: 'data not found',
-//     });
-//   }
-// };
