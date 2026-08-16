671523007f867d6fb93e816d9ae47165421ea1da
Maryam Salimi <salimi.5134@gmail.com>
2026-08-16 15:52:22 +0330

    Fixed Imagekit settings

--- controllers/adController.js
+++ controllers/adController.js
@@ -11,6 +11,7 @@ require('dotenv').config();
 // connect to ImageKit for upload file
 const ImageKit = require('imagekit');
 const imagekit = new ImageKit({
+  publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
   privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
   urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
 });
--- controllers/userController.js
+++ controllers/userController.js
@@ -18,6 +18,7 @@ const { PutObjectCommand } = require('@aws-sdk/client-s3');
 const ImageKit = require('imagekit');
 
 const imagekit = new ImageKit({
+  publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
   privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
   urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
 });
